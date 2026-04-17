45362de334f488b2f3e12de7e8ab1090cd2c535b
sxu22314 <sxu22314@gmail.com>
2026-04-17 14:12:00 +0800

    【功能】实现userAPI跨域访问

--- src/main/java/com/example/linkfamily/Controller/UserController.java
+++ src/main/java/com/example/linkfamily/Controller/UserController.java
@@ -1,14 +1,18 @@
 package com.example.linkfamily.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.linkfamily.Response.Response;
 import com.example.linkfamily.Service.UserService;
 
 @RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "*")
 public class UserController {
     
     @Autowired
